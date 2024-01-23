5bda194a1ac1f04ddfd7d05a9b8ca18686e34d7c
BreadBabe <143431412+BreadBabe@users.noreply.github.com>
2024-01-23 14:09:39 +0100

    Added new movement code and made a simple raycast

--- Assets/MovmentAndCrosshair.cs
+++ Assets/MovmentAndCrosshair.cs
@@ -7,37 +7,46 @@ using UnityEngine.Jobs;
 public class MovementAndCrosshair : MonoBehaviour
 {
     private CharacterController controller;
-    private float playerSpeed = 10.0f;
+    private float legacySpeed;
     public Transform crosshair;
     public Camera playerCamera;
-    Vector2 direction;
+    Vector3 direction;
+    float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
-        
+        speed = 10f;
+        direction = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Rotating rectangle to mouse position
+        //Sets 
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
         transform.up = direction;
         crosshair.position = new Vector3 (mousePosition.x,mousePosition.y,0);
 
-        //Move with wasd and arrow keys
-        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        move.Normalize();
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        //Move code
+        direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
+        transform.Translate(direction * speed * Time.deltaTime);
 
         //Camera
         playerCamera.transform.position = new Vector3(transform.position.x,transform.position.y,-10);
     }
 
+    //Old movement code i am not sure i wanna remove yet
+    void LegacyMovement()
+    {
+        //Move with wasd and arrow keys
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        move.Normalize();
+        controller.Move(move * Time.deltaTime * legacySpeed);
+    }
 
-    
 }
---
+++ Assets/Raycasting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycasting : MonoBehaviour
+{
+
+    //Creates a Raycast
+    RaycastHit2D hit;
+    Vector3 mousePosition;
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        hit = Physics2D.Raycast(transform.position, transform.up);
+        if (hit.collider)
+        {
+            Debug.Log(hit.collider);
+        }
+        //Debug.Log(mousePosition);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawLine(transform.position, Vector2.right);
+        Gizmos.color = Color.green;
+        
+    }
+}
