46978545759c41e80150aeea8aef25e3ff129e7b
paprikathread <74513624+paprikathread@users.noreply.github.com>
2024-01-23 12:27:02 +0100

    Revert "testing map, collision, movement"
    This reverts commit 82b079f0f6920b511659ac865e24286ebc959fb5.

--- Assets/MovmentAndCrosshair.cs
+++ Assets/MovmentAndCrosshair.cs
@@ -6,46 +6,38 @@ using UnityEngine.Jobs;
 
 public class MovementAndCrosshair : MonoBehaviour
 {
+    private CharacterController controller;
+    private float playerSpeed = 10.0f;
     public Transform crosshair;
     public Camera playerCamera;
-
-    private Rigidbody2D rb;
-
-    private float currentSpeed;
-
-    private Vector2 faceDirection;
-    private Vector2 moveDirection;
+    Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        //rb.gravityScale = 0.0f;
-
-        currentSpeed = 5.0f;
+        controller = gameObject.GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Rotating rectangle to mouse position
+        //Rotating rectangle to mouse position
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        faceDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-        transform.up = faceDirection;
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
         crosshair.position = new Vector3 (mousePosition.x,mousePosition.y,0);
 
-        // GetAxis() returns a value of -1, 0 or 1 depending on button clicked, Which button does what can be seen under "input manager" in project settings
-        // Its normalized so that the speed will be consistent even if you are walking diagonaly
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
+        //Move with wasd and arrow keys
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        move.Normalize();
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        // Camera
+        //Camera
         playerCamera.transform.position = new Vector3(transform.position.x,transform.position.y,-10);
     }
 
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveDirection * currentSpeed * Time.fixedDeltaTime);
 
-    }
+    
 }
