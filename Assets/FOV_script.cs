2518c6d8de6f9350095dba7aa758562df2985e00
BreadBabe <143431412+BreadBabe@users.noreply.github.com>
2024-01-26 12:26:57 +0100

    Update FOV_script.cs

--- Assets/FOV_script.cs
+++ Assets/FOV_script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Raycasting : MonoBehaviour
+public class FOV_script : MonoBehaviour
 {
 
     //Creates a Raycast
