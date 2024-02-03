f8a961b207c32de6820f252af30e178c3b8a39b3
Anton Zupic <zupic.anton@gmail.com>
2024-02-03 14:04:32 +0100

    Healthbar
    script för healthbar och UI element

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Health : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+
+    public Healthbar bar;
     
 
     void Start()
     {
         currentHealth = maxHealth;
+        bar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        bar.SetHealth(currentHealth);
         if (currentHealth <= 0) 
         {
             currentHealth = 0;
@@ -28,6 +33,7 @@ public class Health : MonoBehaviour
     void Heal(int health)
     {
         currentHealth += health;
+        bar.SetHealth(currentHealth);
         if (currentHealth >= maxHealth)
         {
             currentHealth = maxHealth;
@@ -35,4 +41,14 @@ public class Health : MonoBehaviour
             
         }
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+
 }
