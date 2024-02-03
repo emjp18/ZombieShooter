b6ba35e561ce559e24a4cc41efd90033aece8697
emjp18 <emjp1821@gmail.com>
2024-02-03 22:06:11 +0100

    zombie damage

--- Assets/scripts/takeDamageScript.cs
+++ Assets/scripts/takeDamageScript.cs
@@ -7,20 +7,28 @@ public class takeDamageScript : MonoBehaviour
     BoxCollider2D box;
     Rigidbody2D rb;
     public float pushbackForce = 2.5f;
+
+    public int healthPoints = 100;
     private void Start()
     {
         box = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         
     }
-    private void FixedUpdate()
+    private void Update()
     {
-        
+        if (healthPoints <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "bullet")
         {
+            //Debug.Log("hit");
+            int damage = collision.gameObject.GetComponent<weapon_DamageScript>().damagePerHit;
+            healthPoints -= damage;
 
             rb.AddForce((collision.transform.position - GameObject.Find("Player").transform.position).normalized * pushbackForce,ForceMode2D.Impulse);
         }
