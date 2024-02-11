130042c2fd6ec81c94d53408c476a47246ef39ba
Gudama1000 <gustav.langes@gmail.com>
2024-02-11 12:49:05 +0100

    Improved how the bullet looks
    Made it a black line instead of a white dot.
    Made it rotate based on the direction its being fired.
    Made its bottom edge be where its being cloned to instead of the center(so that the bullet wont overlap with the player when its being fired)

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
+    private float bulletOffset;
+
     public float reloadTime;
     private float reloadTimer;
 
     void Start()
     {
-
+        //Calculates the offset needed so that the bullet can get cloned at its edge not center (to avoid the bullet overlapping with the player when its being fired)
+        bulletOffset = (bullet.transform.lossyScale.y / 2);
     }
 
     void Update()
@@ -18,8 +21,8 @@ public class Shooting : MonoBehaviour
         reloadTimer += Time.deltaTime;
         if (Input.GetMouseButtonDown(0) && reloadTimer >= reloadTime)
         {
-            //Clones a bullet at the position of the object the script is on(the gun) and then sets it active
-            GameObject bulletInstance = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+            //Clones a bullet at position of the gun + offset * transform.up to the offset independent of rotation, and then sets it active
+            GameObject bulletInstance = Instantiate(bullet, gameObject.transform.position + bulletOffset * transform.up, gameObject.transform.rotation);
             bulletInstance.SetActive(true);
 
             reloadTimer = 0;
