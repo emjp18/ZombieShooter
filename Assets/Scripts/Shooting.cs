90d8b7739652c76f0fe62b83f4c41a95b4657b71
Gudama1000 <gustav.langes@gmail.com>
2024-02-13 16:16:29 +0100

    Camera now shakes when a bullet is fired
    When a bullet is fired a timer is started, during which the camera is offset by a small random amount each frame

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
-    private float bulletOffset;
+
+    private CameraMovement cameraMovement;
+
+    private float spawnOffset;
 
     public float reloadTime;
     private float reloadTimer;
 
     void Start()
     {
+        cameraMovement = Camera.main.GetComponent<CameraMovement>();
+
         //Calculates the offset needed so that the bullet can get cloned at its edge not center (to avoid the bullet overlapping with the player when its being fired)
-        bulletOffset = (bullet.transform.lossyScale.y / 2);
+        spawnOffset = (bullet.transform.lossyScale.y / 2);
     }
 
     void Update()
@@ -22,10 +27,11 @@ public class Shooting : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && reloadTimer >= reloadTime)
         {
             //Clones a bullet at position of the gun + offset * transform.up to the offset independent of rotation, and then sets it active
-            GameObject bulletInstance = Instantiate(bullet, gameObject.transform.position + bulletOffset * transform.up, gameObject.transform.rotation);
+            GameObject bulletInstance = Instantiate(bullet, gameObject.transform.position + spawnOffset * transform.up, gameObject.transform.rotation);
             bulletInstance.SetActive(true);
 
             reloadTimer = 0;
+            cameraMovement.shakeTimer = 0;
         }
     }
 }
--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -6,12 +6,38 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform playerTransform;
 
+    private Vector3 newPosition;
+
+    private float temporaryXOffset;
+    private float temporaryYOffset;
+
+    public float shakeAmount;
+    public float shakeDuration, shakeTimer;
+
     void Start()
     {
+        newPosition.z = -10;
+        shakeTimer = shakeDuration;
     }
 
     void Update()
     {
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10);
+        if (shakeTimer < shakeDuration)
+        {
+            temporaryXOffset = Random.Range(-1, 2) * shakeAmount;
+            temporaryYOffset = Random.Range(-1, 2) * shakeAmount;
+
+            shakeTimer += Time.deltaTime;
+        }
+        else
+        {
+            temporaryXOffset = 0;
+            temporaryYOffset = 0;
+        }
+
+        newPosition.x = playerTransform.position.x + temporaryXOffset;
+        newPosition.y = playerTransform.position.y + temporaryYOffset;
+
+        transform.position = newPosition;
     }
 }
