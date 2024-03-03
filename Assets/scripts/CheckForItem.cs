b868c7deea29e75a136313552aef09534d110e8c
Gudama1000 <gustav.langes@gmail.com>
2024-03-03 11:22:27 +0100

    Created Level_Two. CheckForItem now switches level (not completly) and some improvements
    CheckForItem now only counts items in inventory when a method is run.

--- Assets/scripts/CheckForItem.cs
+++ Assets/scripts/CheckForItem.cs
@@ -2,36 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckForItem : MonoBehaviour
 {
     [SerializeField] private InventoryManagerScript inventoryScript;
-    [SerializeField] string itemName;
-    [SerializeField] int itemCount;
-
-    int currentItemCount;
-
-    void Update()
+    [SerializeField] private string itemName;
+    [SerializeField] private int itemCount;
+    [SerializeField] private string nextLevelName;
+    public int CountNumberOfItem()
     {
-        if (currentItemCount < itemCount)
+        int currentItemCount = 0;
+
+        for (int i = 0; i < inventoryScript.itemSlot.Length - 1; i++)
         {
-            currentItemCount = 0;
-            for (int i = 0; i < inventoryScript.itemSlot.Length - 1; i++)
+            if (inventoryScript.itemSlot[i].itemName == itemName)
             {
-                if (inventoryScript.itemSlot[i].itemName == itemName)
-                {
-                    currentItemCount += inventoryScript.itemSlot[i].quantity;
-                }
+                currentItemCount += inventoryScript.itemSlot[i].quantity;
             }
         }
-    }
 
+        return currentItemCount;
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (currentItemCount >= itemCount)
+        Debug.Log("bam    " + CountNumberOfItem());
+        if (CountNumberOfItem() >= itemCount)
         {
-            //Add win code here
-            Debug.Log("car on!     " + currentItemCount);
+            Debug.Log("should work");
+            SceneManager.LoadScene(nextLevelName);
         }
     }
 }
