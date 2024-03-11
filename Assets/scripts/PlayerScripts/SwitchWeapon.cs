999db448517a36c470dd1080c1c4d5524c0fae1d
Gudama1000 <gustav.langes@gmail.com>
2024-03-11 17:55:26 +0100

    Updated switchweapon script
    Can now change to knife and baseball bat. Does not change textures

--- Assets/scripts/PlayerScripts/SwitchWeapon.cs
+++ Assets/scripts/PlayerScripts/SwitchWeapon.cs
@@ -7,6 +7,8 @@ public class SwitchWeapon : MonoBehaviour
     [SerializeField] private GameObject Gun;
     [SerializeField] private GameObject Rifle;
     [SerializeField] private GameObject FlameThrower;
+    [SerializeField] private GameObject Knife;
+    [SerializeField] private GameObject Bat;
 
     /* USED FOR PLAYER MODEL ANIMATION */
     public Animator playerAnimator;
@@ -23,6 +25,8 @@ public class SwitchWeapon : MonoBehaviour
             Gun.SetActive(true);
             Rifle.SetActive(false);
             FlameThrower.SetActive(false);
+            Knife.SetActive(false);
+            Bat.SetActive(false);
             playerAnimator.SetInteger("WeaponType", 1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -30,6 +34,8 @@ public class SwitchWeapon : MonoBehaviour
             Gun.SetActive(false);
             Rifle.SetActive(true);
             FlameThrower.SetActive(false);
+            Knife.SetActive(false);
+            Bat.SetActive(false);
             playerAnimator.SetInteger("WeaponType", 2);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -37,6 +43,26 @@ public class SwitchWeapon : MonoBehaviour
             Gun.SetActive(false);
             Rifle.SetActive(false);
             FlameThrower.SetActive(true);
+            Knife.SetActive(false);
+            Bat.SetActive(false);
+            playerAnimator.SetInteger("WeaponType", 3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Gun.SetActive(false);
+            Rifle.SetActive(false);
+            FlameThrower.SetActive(false);
+            Knife.SetActive(true);
+            Bat.SetActive(false);
+            playerAnimator.SetInteger("WeaponType", 3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Gun.SetActive(false);
+            Rifle.SetActive(false);
+            FlameThrower.SetActive(false);
+            Knife.SetActive(false);
+            Bat.SetActive(true);
             playerAnimator.SetInteger("WeaponType", 3);
         }
     }
